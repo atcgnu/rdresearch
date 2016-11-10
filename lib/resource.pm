cab314a4ba1d68ed6c770a2711dab0c718813db1
atcgnu <atcgnu@gmail.com>
2016-11-10 16:19:03 +0800

    create resource.pm

---
+++ lib/resource.pm
@@ -0,0 +1,29 @@
+packge resource;
+
+our $VERSION = 'r1.0';
+
+=head1 CONTACT
+
+  Yulan Chen
+
+=head1 NAME
+
+=head1 SYNOPSIS
+
+=head2 validate
+
+=head1 DESCRIPTION
+
+=head1 BUGS
+
+Feedback suggestions and bug reports for resource.pm to https://github.com/atcgnu/rdresearch/issues. Please include the version of resource.pm, and the error information.
+
+=head1 COPYRIGHT AND LICENSE
+
+This software is Copyright (c) 2016 by Beijing Genomics Institute at Shenzhen.
+
+This is free software, licensed under:
+
+The GNU General Public License, Version 3, June 2007
+
+=cut
