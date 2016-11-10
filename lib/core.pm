9447b6c6b302fafa2dabe711b48b2ae113ec3053
atcgnu <atcgnu@gmail.com>
2016-11-10 16:17:26 +0800

    create core.pm

---
+++ lib/core.pm
@@ -0,0 +1,29 @@
+packge core;
+
+our $VERSION = 'r1.0';
+
+=head1 CONTACT
+
+  Yulan Chen <shenyulan@genomics.cn>
+
+=head1 NAME
+
+=head1 SYNOPSIS
+
+=head2 validate
+
+=head1 DESCRIPTION
+
+=head1 BUGS
+
+Feedback suggestions and bug reports for core.pm to https://github.com/atcgnu/rdresearch/issues. Please include the version of come.pm, and the error information.
+
+=head1 COPYRIGHT AND LICENSE
+
+This software is Copyright (c) 2016 by Beijing Genomics Institute at Shenzhen.
+
+This is free software, licensed under:
+
+The GNU General Public License, Version 3, June 2007
+
+=cut
