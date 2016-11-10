d97830a8cae0690deee1d962d8ef2a030c583c38
atcgnu <atcgnu@gmail.com>
2016-11-10 16:15:04 +0800

    create build.pm

---
+++ lib/build.pm
@@ -0,0 +1,29 @@
+packge build;
+
+our $VERSION = 'r1.0';
+
+=head1 CONTACT
+
+  Yulan Chen <shenyulan@genomics.cn>
+
+=head1 NAME
+
+=head1 SYNOPSIS
+
+=head2 validate
+
+=head1 DESCRIPTION
+
+=head1 BUGS
+
+Mail suggestions and bug reports for build.pm to shenyulan@genomics.cn. Please include the version of build.pm, and the error information.
+
+=head1 COPYRIGHT AND LICENSE
+
+This software is Copyright (c) 2016 by Beijing Genomics Institute at Shenzhen.
+
+This is free software, licensed under:
+
+The GNU General Public License, Version 3, June 2007
+
+=cut
